900d73c1b5e967ac3ac55d729b7576ac2f69f85a
Misha Ramendik <mramendi@redhat.com>
2023-09-05 19:34:29 +0100

    added gitlab retry in setup-repos.sh

--- setup-repos.sh
+++ setup-repos.sh
@@ -4,6 +4,7 @@ cd repos
 if [ -z $1 ] ; then
   git clone git@gitlab.cee.redhat.com:red-hat-enterprise-openshift-documentation/layered-products-docs.git
 else
-  git clone https://clone:"$1"@gitlab.cee.redhat.com/red-hat-enterprise-openshift-documentation/layered-products-docs.git
+# this option has a retry, because sometimes Gitlab fails with error 504
+  git clone https://clone:"$1"@gitlab.cee.redhat.com/red-hat-enterprise-openshift-documentation/layered-products-docs.git || git clone https://clone:"$1"@gitlab.cee.redhat.com/red-hat-enterprise-openshift-documentation/layered-products-docs.git
 fi
 git clone https://github.com/openshift/openshift-docs.git --depth=1 --no-single-branch
